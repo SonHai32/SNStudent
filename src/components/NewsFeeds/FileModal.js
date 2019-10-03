d749516a6305f40ce425d34bf04523bb0cfbae77
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-10-03 15:44:20 -0400

    add component display error

--- src/components/NewsFeeds/FileModal.js
+++ src/components/NewsFeeds/FileModal.js
@@ -1,38 +1,65 @@
 import React from 'react'
 import { Modal, Input, Button, Icon } from 'semantic-ui-react';
-
+import DisplayError from '../Error/DisplayError'
 
 class FileModal extends React.Component{
     state = {
         authorized: ['image/jpeg', 'image/png', 'image/webp'],
-        currentFiles: []
+        currentFiles: [],
+        errorModal: false,
+        errorMessage: ''
 
     }
 
-   
+// openErrorModal = () =>{
+//     this.setState({errorModal: true})
+// }
+
+   closeErrorModal = () =>{
+       this.setState({errorModal: false},()=>{
+           this.setState({errorMessage: ''})
+       })
+   }
+
+
+
+
     addFile = event =>{
     
-        
+        this.setState({currentFiles: []})
         const files = event.target.files;
         
         
    
         if(files){
-            if(files.length <= 20 ){
-                for(let i = 0 ; i < files.length ; i++){
-                    const metadata = {contentType: files[i].type}
-                    const reader = new FileReader();
-                    if(this.isAuthorized(files[i].type)){
-                        reader.onload = event =>{
-                            const file = {file: files[i], url: event.target.result, metadata: metadata, key:i} 
-                            this.state.currentFiles.push(file)
-                        }
-                        reader.readAsDataURL(files[i])
+           
+                if(files.length <= 50 ){
+                    for(let i = 0 ; i < files.length ; i++){
+                        const metadata = {contentType: files[i].type}
+                        const reader = new FileReader();
+                        if(this.isAuthorized(files[i].type)){
+                            
+                            if(files[i].size <= 3355443){
+                                reader.onload = event =>{
+                                    const file = {file: files[i], url: event.target.result, metadata: metadata, key:files[i].name} 
+                                    this.state.currentFiles.push(file)
+                                }
+                                
+                                reader.readAsDataURL(files[i])
+                            }else{
+                                this.setState({errorMessage: 'Ảnh không được vượt quá 3.2MB'})
+                            }
+                    }else{
+                        this.setState({errorMessage: `${files[i].name} không thể tải lên.Vui lòng tải ảnh có định dạng (PNG/JPGE)`})
                     }
                 }
-            }
-        }
-    }
+                }else{
+                    this.setState({errorMessage: 'Chỉ được đăng tối đa 50 ảnh'})}
+            
+                
+        
+        
+    }}
     
 
 
@@ -41,9 +68,14 @@ class FileModal extends React.Component{
     sendFileToProp = () =>{
       
      const {fileStateToProp,closeModal} = this.props
-         fileStateToProp(this.state.currentFiles);
-         this.clearFile();
-         closeModal();
+         if(this.state.currentFiles.length > 0 ){
+            fileStateToProp(this.state.currentFiles);
+            this.clearFile();
+            closeModal();
+         }else{
+            this.setState({errorMessage: 'Bạn chưa chọn ảnh'})
+         }
+         
 
     }
 
@@ -67,6 +99,9 @@ class FileModal extends React.Component{
      
 
        return(
+           <React.Fragment>
+               <DisplayError errorModal={this.state.errorMessage.length > 0} closeModal={this.closeErrorModal} error={this.state.errorMessage} />
+           
             <Modal basic open={fileModal} onClose={closeModal}>
                 <Modal.Header>
                     Select an image file
@@ -96,6 +131,7 @@ class FileModal extends React.Component{
                     
                 </Modal.Actions>
             </Modal>
+            </React.Fragment>
        );
     }
 }
